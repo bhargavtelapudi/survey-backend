39dd94da77941683e90f780aa52105d2fe7e3e73
elizabethrani <elizabethrani.madanu@eagles.oc.edu>
2022-07-04 11:16:53 -0500

    protect route added

---
+++ protect.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+
+verifyToken = async (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({
+      message: "token required!"
+    });
+  }
+
+  await jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!"
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+isSuperAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    if (user == null) {
+      return res.status(401).send({
+        message: "Unauthorized!"
+      });
+    }
+    if (user.userType === "super_admin") {
+      next();
+      return;
+    }
+
+
+    res.status(403).send({
+      message: " super_admin can only access this api!"
+    });
+    return;
+
+  });
+};
+
+
+module.exports = {verifyToken,isSuperAdmin}
