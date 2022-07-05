95fb98a0826483ec8bbb7763cb728a3b7cb2d57b
bhargavtelapudi <62876599+bhargav443@users.noreply.github.com>
2022-07-05 09:57:27 -0500

    done

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -2,5 +2,14 @@ const helper = require("../services/helper")
 const controller = require("../controllers/auth_controller");
 const admin_controller = require("../controllers/admin_controller")
 const protect = require("../protect")
-
+module.exports = function (app) {
+    app.use(function (req, res, next) {
+      res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+      );
+      next();
+    });
+  
 app.get("/api/admin/userlist", [protect.verifyToken,protect.isSuperAdmin], admin_controller.users_list);
+}
