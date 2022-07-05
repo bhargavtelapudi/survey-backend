d93fc4fadbcac268dfea7b70a3e9778f0ee70884
bhargavtelapudi <62876599+bhargav443@users.noreply.github.com>
2022-07-05 14:02:18 -0500

    get survey list

--- routes/user_route.js
+++ routes/user_route.js
@@ -13,5 +13,6 @@ module.exports = function (app) {
   
 app.get("/api/admin/userlist", [protect.verifyToken,protect.isSuperAdmin], admin_controller.users_list);
 app.post("/api/create/survey", [protect.verifyToken], admin_controller.create_survey);
+app.get("/api/survey/list", [protect.verifyToken], admin_controller.survey_list);
 
 }
